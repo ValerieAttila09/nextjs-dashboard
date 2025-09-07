e88d6b434b2beb5bbc8bd2ac26831e7275af0713
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-09-07 17:39:03 +0000

    committed

--- app/api/delete-user/[id]/route.ts
+++ app/api/delete-user/[id]/route.ts
@@ -1,14 +1,12 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export const DELETE = async (
+export async function DELETE(
   req: NextRequest,
-  context: { params: { id: string } } | { params: Promise<{ id: string }> }
-) => {
+  context: { params: { id: string } }
+) {
   try {
-    // Handle if params is a Promise (for Vercel/Next.js 14+)
-    const params = "then" in context.params ? await context.params : context.params;
-    const { id } = params;
+    const { id } = context.params;
 
     const user = await prisma.user.delete({
       where: { id }
