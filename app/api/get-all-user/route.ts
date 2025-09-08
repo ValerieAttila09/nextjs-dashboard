bae2d3c115ff4f374532f9fe15fcebef0db80b06
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-09-08 15:14:41 +0000

    committed

--- app/api/get-all-user/route.ts
+++ app/api/get-all-user/route.ts
@@ -1,15 +1,11 @@
 import prisma from "@/prisma/client";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export async function GET() {
   try {
-    const users = await prisma.user.findMany()
-
-    return NextResponse.json(users, { status: 200 })
-
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users, { status: 200 });
   } catch (error) {
-    return NextResponse.json({
-      error: "Failed to fetch user!"
-    }, { status: 500 })
+    return NextResponse.json({ error: "Failed to fetch users!" }, { status: 500 });
   }
 }
