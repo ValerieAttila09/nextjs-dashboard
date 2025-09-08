8b40abf576da9cff37b2d9c470e1179c071a10fd
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-09-08 14:38:14 +0000

    committed

--- app/api/delete-user/[id]/route.ts
+++ app/api/delete-user/[id]/route.ts
@@ -1,22 +1,36 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
+export async function PUT(
   req: NextRequest,
   context: { params: any }
 ) {
   try {
     const { id } = context.params;
+    const body = await req.json();
+    const { username, email, password } = body;
 
-    const user = await prisma.user.delete({
-      where: { id }
+    if (!username && !email && !password) {
+      return NextResponse.json(
+        { error: "At least one field (username, email, password) is required!" },
+        { status: 400 }
+      );
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        ...(username && { username }),
+        ...(email && { email }),
+        ...(password && { password }),
+      },
     });
 
-    return NextResponse.json(user, { status: 200 });
-
+    return NextResponse.json(updatedUser, { status: 200 });
   } catch (error) {
-    return NextResponse.json({
-      error: "Failed to delete user!"
-    }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to update user!" },
+      { status: 500 }
+    );
   }
 }
