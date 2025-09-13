07c4757abfe64df465b357559c583f57c6465e9e
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-09-13 06:18:28 +0000

    sidebar animation complete

---
+++ app/statistics/page.tsx
@@ -0,0 +1,344 @@
+"use client"
+
+import * as React from "react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import Sidebar from "@/components/Navbar/Sidebar"
+import Link from "next/link"
+import { Plus, Search } from "lucide-react"
+import SidebarToggle from "@/components/SidebarToggle/SidebarToggle"
+import { Input } from "@/components/ui/input"
+import { useRef, useState } from "react"
+import gsap from "gsap"
+
+export const description = "An interactive area chart"
+
+const chartData = [
+  { date: "2024-04-01", desktop: 222, mobile: 150 },
+  { date: "2024-04-02", desktop: 97, mobile: 180 },
+  { date: "2024-04-03", desktop: 167, mobile: 120 },
+  { date: "2024-04-04", desktop: 242, mobile: 260 },
+  { date: "2024-04-05", desktop: 373, mobile: 290 },
+  { date: "2024-04-06", desktop: 301, mobile: 340 },
+  { date: "2024-04-07", desktop: 245, mobile: 180 },
+  { date: "2024-04-08", desktop: 409, mobile: 320 },
+  { date: "2024-04-09", desktop: 59, mobile: 110 },
+  { date: "2024-04-10", desktop: 261, mobile: 190 },
+  { date: "2024-04-11", desktop: 327, mobile: 350 },
+  { date: "2024-04-12", desktop: 292, mobile: 210 },
+  { date: "2024-04-13", desktop: 342, mobile: 380 },
+  { date: "2024-04-14", desktop: 137, mobile: 220 },
+  { date: "2024-04-15", desktop: 120, mobile: 170 },
+  { date: "2024-04-16", desktop: 138, mobile: 190 },
+  { date: "2024-04-17", desktop: 446, mobile: 360 },
+  { date: "2024-04-18", desktop: 364, mobile: 410 },
+  { date: "2024-04-19", desktop: 243, mobile: 180 },
+  { date: "2024-04-20", desktop: 89, mobile: 150 },
+  { date: "2024-04-21", desktop: 137, mobile: 200 },
+  { date: "2024-04-22", desktop: 224, mobile: 170 },
+  { date: "2024-04-23", desktop: 138, mobile: 230 },
+  { date: "2024-04-24", desktop: 387, mobile: 290 },
+  { date: "2024-04-25", desktop: 215, mobile: 250 },
+  { date: "2024-04-26", desktop: 75, mobile: 130 },
+  { date: "2024-04-27", desktop: 383, mobile: 420 },
+  { date: "2024-04-28", desktop: 122, mobile: 180 },
+  { date: "2024-04-29", desktop: 315, mobile: 240 },
+  { date: "2024-04-30", desktop: 454, mobile: 380 },
+  { date: "2024-05-01", desktop: 165, mobile: 220 },
+  { date: "2024-05-02", desktop: 293, mobile: 310 },
+  { date: "2024-05-03", desktop: 247, mobile: 190 },
+  { date: "2024-05-04", desktop: 385, mobile: 420 },
+  { date: "2024-05-05", desktop: 481, mobile: 390 },
+  { date: "2024-05-06", desktop: 498, mobile: 520 },
+  { date: "2024-05-07", desktop: 388, mobile: 300 },
+  { date: "2024-05-08", desktop: 149, mobile: 210 },
+  { date: "2024-05-09", desktop: 227, mobile: 180 },
+  { date: "2024-05-10", desktop: 293, mobile: 330 },
+  { date: "2024-05-11", desktop: 335, mobile: 270 },
+  { date: "2024-05-12", desktop: 197, mobile: 240 },
+  { date: "2024-05-13", desktop: 197, mobile: 160 },
+  { date: "2024-05-14", desktop: 448, mobile: 490 },
+  { date: "2024-05-15", desktop: 473, mobile: 380 },
+  { date: "2024-05-16", desktop: 338, mobile: 400 },
+  { date: "2024-05-17", desktop: 499, mobile: 420 },
+  { date: "2024-05-18", desktop: 315, mobile: 350 },
+  { date: "2024-05-19", desktop: 235, mobile: 180 },
+  { date: "2024-05-20", desktop: 177, mobile: 230 },
+  { date: "2024-05-21", desktop: 82, mobile: 140 },
+  { date: "2024-05-22", desktop: 81, mobile: 120 },
+  { date: "2024-05-23", desktop: 252, mobile: 290 },
+  { date: "2024-05-24", desktop: 294, mobile: 220 },
+  { date: "2024-05-25", desktop: 201, mobile: 250 },
+  { date: "2024-05-26", desktop: 213, mobile: 170 },
+  { date: "2024-05-27", desktop: 420, mobile: 460 },
+  { date: "2024-05-28", desktop: 233, mobile: 190 },
+  { date: "2024-05-29", desktop: 78, mobile: 130 },
+  { date: "2024-05-30", desktop: 340, mobile: 280 },
+  { date: "2024-05-31", desktop: 178, mobile: 230 },
+  { date: "2024-06-01", desktop: 178, mobile: 200 },
+  { date: "2024-06-02", desktop: 470, mobile: 410 },
+  { date: "2024-06-03", desktop: 103, mobile: 160 },
+  { date: "2024-06-04", desktop: 439, mobile: 380 },
+  { date: "2024-06-05", desktop: 88, mobile: 140 },
+  { date: "2024-06-06", desktop: 294, mobile: 250 },
+  { date: "2024-06-07", desktop: 323, mobile: 370 },
+  { date: "2024-06-08", desktop: 385, mobile: 320 },
+  { date: "2024-06-09", desktop: 438, mobile: 480 },
+  { date: "2024-06-10", desktop: 155, mobile: 200 },
+  { date: "2024-06-11", desktop: 92, mobile: 150 },
+  { date: "2024-06-12", desktop: 492, mobile: 420 },
+  { date: "2024-06-13", desktop: 81, mobile: 130 },
+  { date: "2024-06-14", desktop: 426, mobile: 380 },
+  { date: "2024-06-15", desktop: 307, mobile: 350 },
+  { date: "2024-06-16", desktop: 371, mobile: 310 },
+  { date: "2024-06-17", desktop: 475, mobile: 520 },
+  { date: "2024-06-18", desktop: 107, mobile: 170 },
+  { date: "2024-06-19", desktop: 341, mobile: 290 },
+  { date: "2024-06-20", desktop: 408, mobile: 450 },
+  { date: "2024-06-21", desktop: 169, mobile: 210 },
+  { date: "2024-06-22", desktop: 317, mobile: 270 },
+  { date: "2024-06-23", desktop: 480, mobile: 530 },
+  { date: "2024-06-24", desktop: 132, mobile: 180 },
+  { date: "2024-06-25", desktop: 141, mobile: 190 },
+  { date: "2024-06-26", desktop: 434, mobile: 380 },
+  { date: "2024-06-27", desktop: 448, mobile: 490 },
+  { date: "2024-06-28", desktop: 149, mobile: 200 },
+  { date: "2024-06-29", desktop: 103, mobile: 160 },
+  { date: "2024-06-30", desktop: 446, mobile: 400 },
+]
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig
+
+export default function Statistics() {
+  const [timeRange, setTimeRange] = React.useState("90d")
+  const [active, setActive] = useState(true)
+  const sidebarRef = useRef<HTMLDivElement>(null)
+  const menuRefs = useRef<Array<HTMLSpanElement | null>>([])
+
+
+  const filteredData = chartData.filter((item) => {
+    const date = new Date(item.date)
+    const referenceDate = new Date("2024-06-30")
+    let daysToSubtract = 90
+    if (timeRange === "30d") {
+      daysToSubtract = 30
+    } else if (timeRange === "7d") {
+      daysToSubtract = 7
+    }
+    const startDate = new Date(referenceDate)
+    startDate.setDate(startDate.getDate() - daysToSubtract)
+    return date >= startDate
+  })
+
+
+  const toggleSidebar = () => {
+    if (sidebarRef.current) {
+      gsap.to(sidebarRef.current, {
+        width: active ? "60px" : "280px",
+        ease: "power2.out",
+        duration: 0.4,
+      })
+      // Animasi semua nama menu
+      gsap.to(menuRefs.current, {
+        opacity: active ? 0 : 1,
+        width: active ? 0 : "auto",
+        marginLeft: active ? 0 : "8px",
+        stagger: 0.03,
+        ease: "power2.out",
+        duration: 0.3,
+        onStart: () => {
+          if (active) {
+            menuRefs.current.forEach((el) => {
+              if (el) el.style.overflow = "hidden"
+            })
+          }
+        },
+        onComplete: () => {
+          if (!active) {
+            menuRefs.current.forEach((el) => {
+              if (el) el.style.overflow = ""
+            })
+          }
+        }
+      })
+      setActive(!active)
+    }
+  }
+
+  return (
+    <div className="w-full flex">
+      <Sidebar
+        element={sidebarRef}
+        isDashboard={false}
+        classes={"h-screen"}
+        menuRefs={menuRefs}
+      />
+
+      <div className="w-full pe-4">
+        <div className="flex items-center justify-between py-2">
+          <div className="flex items-center gap-2">
+            <SidebarToggle toggleSidebar={toggleSidebar} classes={""} />
+            <div className="relative w-auto">
+              <Input type="text" className="bg-white ps-[30px] text-neutral-800" placeholder="Search" />
+              <Search size={16} color="#898989" className="absolute z-5 -translate-y-[26px] translate-x-2" />
+            </div>
+          </div>
+          <div className="">
+            <Link href={"/create-user"} className="flex items-center h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50">
+              <Plus size={14} color="#898989" className="" />
+              <span className="text-sm text-neutral-800">Add User</span>
+            </Link>
+          </div>
+        </div>
+        <div className="h-full bg-white px-8 py-5 rounded-t-xl border border-input">
+
+          <h1 className="mb-5 text-4xl font-medium text-neutral-900">Users Statistics</h1>
+
+          <Card className="pt-0">
+            <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+              <div className="grid flex-1 gap-1">
+                <CardTitle>Area Chart - Interactive</CardTitle>
+                <CardDescription>
+                  Showing total visitors for the last 3 months
+                </CardDescription>
+              </div>
+              <Select value={timeRange} onValueChange={setTimeRange}>
+                <SelectTrigger
+                  className="hidden w-[160px] rounded-lg sm:ml-auto sm:flex"
+                  aria-label="Select a value"
+                >
+                  <SelectValue placeholder="Last 3 months" />
+                </SelectTrigger>
+                <SelectContent className="rounded-xl">
+                  <SelectItem value="90d" className="rounded-lg">
+                    Last 3 months
+                  </SelectItem>
+                  <SelectItem value="30d" className="rounded-lg">
+                    Last 30 days
+                  </SelectItem>
+                  <SelectItem value="7d" className="rounded-lg">
+                    Last 7 days
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </CardHeader>
+            <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+              <ChartContainer
+                config={chartConfig}
+                className="aspect-auto h-[250px] w-full"
+              >
+                <AreaChart data={filteredData}>
+                  <defs>
+                    <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                      <stop
+                        offset="5%"
+                        stopColor="var(--color-desktop)"
+                        stopOpacity={0.8}
+                      />
+                      <stop
+                        offset="95%"
+                        stopColor="var(--color-desktop)"
+                        stopOpacity={0.1}
+                      />
+                    </linearGradient>
+                    <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                      <stop
+                        offset="5%"
+                        stopColor="var(--color-mobile)"
+                        stopOpacity={0.8}
+                      />
+                      <stop
+                        offset="95%"
+                        stopColor="var(--color-mobile)"
+                        stopOpacity={0.1}
+                      />
+                    </linearGradient>
+                  </defs>
+                  <CartesianGrid vertical={false} />
+                  <XAxis
+                    dataKey="date"
+                    tickLine={false}
+                    axisLine={false}
+                    tickMargin={8}
+                    minTickGap={32}
+                    tickFormatter={(value) => {
+                      const date = new Date(value)
+                      return date.toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                      })
+                    }}
+                  />
+                  <ChartTooltip
+                    cursor={false}
+                    content={
+                      <ChartTooltipContent
+                        labelFormatter={(value) => {
+                          return new Date(value).toLocaleDateString("en-US", {
+                            month: "short",
+                            day: "numeric",
+                          })
+                        }}
+                        indicator="dot"
+                      />
+                    }
+                  />
+                  <Area
+                    dataKey="mobile"
+                    type="natural"
+                    fill="url(#fillMobile)"
+                    stroke="var(--color-mobile)"
+                    stackId="a"
+                  />
+                  <Area
+                    dataKey="desktop"
+                    type="natural"
+                    fill="url(#fillDesktop)"
+                    stroke="var(--color-desktop)"
+                    stackId="a"
+                  />
+                  <ChartLegend content={<ChartLegendContent />} />
+                </AreaChart>
+              </ChartContainer>
+            </CardContent>
+          </Card>
+
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/Navbar/Sidebar.tsx
+++ components/Navbar/Sidebar.tsx
@@ -1,94 +1,59 @@
-import { ArchiveIcon, BoxIcon, ChartBar, FileAxis3dIcon, LineChartIcon, SquareStack, User2Icon, Wallet2Icon } from "lucide-react";
+import { ArchiveIcon, BoxIcon, ChartBar, FileAxis3dIcon, LineChartIcon, SquareStack, User2Icon, Wallet2Icon, ZapIcon } from "lucide-react";
 import { Button } from "../ui/button";
-
-function DashboardMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <ChartBar size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Dashboard</span>
-    </Button>
-  )
-}
-function JobsMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <SquareStack size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Jobs</span>
-    </Button>
-  )
-}
-function StatisticsMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <LineChartIcon size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Statistics</span>
-    </Button>
-  )
-}
-function UsersMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <User2Icon size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Users</span>
-    </Button>
-  )
-}
-function ContainerMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <BoxIcon size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Container</span>
-    </Button>
-  )
-}
-function DocumentMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <FileAxis3dIcon size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Document</span>
-    </Button>
-  )
-}
-function ArchiveMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <ArchiveIcon size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Archive</span>
-    </Button>
-  )
-}
-function WalletMenu({ menu }: { menu: any }) {
-  return (
-    <Button size={"default"} variant={"ghost"} className="overflow-hidden w-full flex items-center justify-start gap-2">
-      <Wallet2Icon size={20} color="#525252" className="" />
-      <span ref={menu} className="text-md text-neutral-800 outfit-regular">Wallet</span>
-    </Button>
-  )
-}
+import Link from "next/link";
+import { useRef } from "react";
 
 export default function Sidebar({
-  isDashboard, classes, element, menu
+  isDashboard, classes, element, menuRefs
 }: {
   isDashboard: boolean,
   classes: any,
   element: any,
-  menu: any
+  menuRefs: React.MutableRefObject<Array<HTMLSpanElement | null>>
 }) {
-
-  const menuList = [<DashboardMenu menu={undefined}/>, <JobsMenu menu={undefined}/>, <StatisticsMenu menu={undefined}/>, <UsersMenu menu={undefined}/>, <ContainerMenu menu={undefined}/>, <DocumentMenu menu={undefined}/>, <ArchiveMenu menu={undefined}/>, <WalletMenu menu={undefined}/>]
+  const menus = [
+    { icon: <ChartBar size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Dashboard", href: "/dashboard" },
+    { icon: <SquareStack size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Jobs", href: "/" },
+    { icon: <LineChartIcon size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Statistics", href: "/statistics" },
+    { icon: <User2Icon size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Users", href: "/" },
+    { icon: <BoxIcon size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Container", href: "/" },
+    { icon: <FileAxis3dIcon size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Document", href: "/" },
+    { icon: <ArchiveIcon size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Archive", href: "/" },
+    { icon: <Wallet2Icon size={20} color="#525252" className="min-h-[20px] min-w-[20px]" />, label: "Wallet", href: "/" },
+  ]
 
   return (
     <div
       ref={element}
-      className={`${classes} w-[280px] relative`}
+      className={`${classes} w-[280px] relative overflow-hidden`}
       style={{ minWidth: 0 }}
     >
-      <div className="grid gap-2 py-2 px-2">
-        {menuList.map((menu, index) => (
-          <div key={index + 1}>
-            {menu}
+      <div className="flex flex-col gap-2 py-2 px-2">
+        <div className="grid gap-2">
+          <div className="py-4 px-1 flex items-center justify-start gap-2">
+            <div className="size-8 rounded-full p-1 bg-white flex items-center justify-center">
+              <ZapIcon size={20} color="#343434"/>
+            </div>
           </div>
-        ))}
+          <div className="grid gap-2">
+            {menus.map((menu, idx) => (
+              <Link
+                key={menu.label}
+                href={menu.href}
+                className="h-9 px-4 py-2 has-[>svg]:px-3 hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/80 overflow-hidden w-full flex items-center justify-start gap-2"
+              >
+                {menu.icon}
+                <span
+                  ref={el => { menuRefs.current[idx] = el; }}
+                  className="text-md text-neutral-800 outfit-regular"
+                  style={{ display: "inline-block", transition: "width 0.3s" }}
+                >
+                  {menu.label}
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
